d447fa674e11bbf68e3f18ea4fefdd10d7ef6877
4grel4 <gabriel_agrela2020@hotmail.com>
2023-04-26 20:35:26 -0300

    Adicionando Aula 2

---
+++ Aula2/atv2.js
@@ -0,0 +1,10 @@
+var n1 = Number(prompt("Digite o primeiro número:"));
+var n2 = Number(prompt("Digite o segundo número:"));
+var n3 = Number(prompt("Digite o terceiro número:"));
+var n4 = Number(prompt("Digite o quarto número:"));
+var n5 = Number(prompt("Digite o quinto número:"));
+
+var soma = n1 + n2 + n3 + n4 + n5;
+var media = soma / 5;
+
+alert("A soma dos números é: " + soma + "\nA média dos números é: " + media);
